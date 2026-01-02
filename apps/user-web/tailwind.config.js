8580ad36a9ed67b332263159908ad7bd7c5ac485
DaDDy-Chill <blacklistuser420@gmail.com>
2026-01-02 20:24:23 +0630

    update color theme admin

--- apps/user-web/tailwind.config.js
+++ apps/user-web/tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     "../../packages/ui/src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+        primary: "var(--primary)",
+        "primary-foreground": "var(--primary-foreground)",
+        secondary: "var(--secondary)",
+        "secondary-foreground": "var(--secondary-foreground)",
+        accent: "var(--accent)",
+        "accent-foreground": "var(--accent-foreground)",
+        destructive: "var(--destructive)",
+        muted: "var(--muted)",
+        "muted-foreground": "var(--muted-foreground)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        card: "var(--card)",
+        "card-foreground": "var(--card-foreground)",
+        popover: "var(--popover)",
+        "popover-foreground": "var(--popover-foreground)",
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "var(--ring)",
+      },
+    },
   },
   plugins: [],
 };
